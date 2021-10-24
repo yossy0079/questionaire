8e955d9b8444474b214b628069dc20cd49865acf
yossy0079 <73327617+yossy0079@users.noreply.github.com>
2021-10-24 20:50:11 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+//using selectors inside the element
+// traversing the dom
+
+const questions = document.querySelectorAll('.question')
+
+questions.forEach(function (question) {
+    console.log(question);
+    const btn = question.querySelector('.question-btn')
+    // console.log(btn)
+    btn.addEventListener('click', function () {
+        question.classList.toggle('show-text')
+    })
+});
+
+
+// const btns = document.querySelectorAll('.question-btn');
+// btns.forEach(function (btn) {
+//     btn.addEventListener('click', function (e) {
+//         const question = e.currentTarget.parentElement.parentElement
+//         question.classList.toggle('show-text')
+//     })
+// })
+
